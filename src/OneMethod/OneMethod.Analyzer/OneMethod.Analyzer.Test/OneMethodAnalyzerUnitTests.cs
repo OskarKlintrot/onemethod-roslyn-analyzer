ff8f3e231f388b61e2f8cd865fab075ce36961c8
Oskar Klintrot <oskar.klintrot@gmail.com>
2021-03-11 14:03:08 +0100

    feat: Warn if more than one public method

--- src/OneMethod/OneMethod.Analyzer/OneMethod.Analyzer.Test/OneMethodAnalyzerUnitTests.cs
+++ src/OneMethod/OneMethod.Analyzer/OneMethod.Analyzer.Test/OneMethodAnalyzerUnitTests.cs
@@ -1,28 +1,27 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using VerifyCS = OneMethod.Analyzer.Test.CSharpCodeFixVerifier<
-    OneMethod.Analyzer.OneMethodAnalyzerAnalyzer,
-    OneMethod.Analyzer.OneMethodAnalyzerCodeFixProvider>;
+using VerifyCS = OneMethod.Analyzer.Test.CSharpAnalyzerVerifier<OneMethod.Analyzer.OneMethodAnalyzer>;
 
 namespace OneMethod.Analyzer.Test
 {
     [TestClass]
     public class OneMethodAnalyzerUnitTest
     {
+        private const string _diagnosticsId = "OM0001";
+
         //No diagnostics expected to show up
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task Empty_Ok()
         {
-            var test = @"";
+            const string test = @"";
 
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
 
-        //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
-        public async Task TestMethod2()
+        public async Task OnePublic_OneInternal_NotOk()
         {
-            var test = @"
+            const string test = @"
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -32,12 +31,22 @@ namespace OneMethod.Analyzer.Test
 
     namespace ConsoleApplication1
     {
-        class {|#0:TypeName|}
-        {   
+        public class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            internal void DoInternal(){ }
+            private void DoPrivate(){ }
         }
     }";
 
-            var fixtest = @"
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task TwoPublic_NotOk()
+        {
+            const string test = @"
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -47,13 +56,207 @@ namespace OneMethod.Analyzer.Test
 
     namespace ConsoleApplication1
     {
-        class TYPENAME
-        {   
+        public class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            public void Do2(){ }
         }
     }";
 
-            var expected = VerifyCS.Diagnostic("OneMethodAnalyzer").WithLocation(0).WithArguments("TypeName");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task TwoInternal_NotOk()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class {|#0:TypeName|}
+        {
+            internal void Do(){ }
+            internal void Do2(){ }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task InternalClass_NotOk()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        internal class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            public void Do2(){ }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task AbstractClass_TwoPublic_NotOk()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public abstract class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            public void Do2(){ }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task StaticClass_TwoPublic_Ok()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public static class {|#0:TypeName|}
+        {
+            public static void Do(){ }
+            public static void Do2(){ }
+        }
+    }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TwoStaticPublic_NotOk()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class {|#0:TypeName|}
+        {
+            public static void Do(){ }
+            public static void Do2(){ }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task Program_OnePublicAndMain_NotOk()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApp1
+    {
+        class {|#0:Program|}
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(""Hello World!"");
+            }
+
+            public void Test()
+            {
+
+            }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(_diagnosticsId).WithLocation(0).WithArguments("Program");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task OnePublicMethod_Ok()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            protected void DoInternal(){ }
+            private void DoPrivate(){ }
+        }
+    }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task OnePublicMethod_OnePublicProperty_Ok()
+        {
+            const string test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class {|#0:TypeName|}
+        {
+            public void Do(){ }
+            public string Foo { get; }
+        }
+    }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
         }
     }
 }
--- src/OneMethod/OneMethod.Analyzer/OneMethod.Analyzer/OneMethodAnalyzer.cs
+++ src/OneMethod/OneMethod.Analyzer/OneMethod.Analyzer/OneMethodAnalyzer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -11,16 +9,16 @@ using Microsoft.CodeAnalysis.Diagnostics;
 namespace OneMethod.Analyzer
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class OneMethodAnalyzerAnalyzer : DiagnosticAnalyzer
+    public class OneMethodAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "OneMethodAnalyzer";
+        public const string DiagnosticId = "OM0001";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Desgin";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
@@ -31,23 +29,37 @@ namespace OneMethod.Analyzer
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.ClassDeclaration);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            if (context.Node is ClassDeclarationSyntax node
+                && !node.Modifiers.Any(x => x.ValueText.Equals(SyntaxFactory.Token(SyntaxKind.StaticKeyword).ValueText)))
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                var keywords = new[] {
+                    SyntaxFactory.Token(SyntaxKind.PublicKeyword).ValueText,
+                    SyntaxFactory.Token(SyntaxKind.InternalKeyword).ValueText,
+                };
 
-                context.ReportDiagnostic(diagnostic);
+                var members = node.Members
+                    .OfType<MethodDeclarationSyntax>()
+                    .ToList();
+
+                var limit = node.Identifier.ValueText.Equals("program", StringComparison.OrdinalIgnoreCase)
+                    && members.Count(x => x.Identifier.ValueText.Equals("main", StringComparison.OrdinalIgnoreCase))
+                    > 0 ? 1 : 2;
+
+                if (members.Count(x => x.Modifiers.Any(y => keywords.Contains(y.ValueText))) >= limit)
+                {
+                    var diagnostic = Diagnostic.Create(
+                        Rule,
+                        node.Identifier.GetLocation(),
+                        node.Identifier.ValueText);
+
+                    context.ReportDiagnostic(diagnostic);
+                }
             }
         }
     }
